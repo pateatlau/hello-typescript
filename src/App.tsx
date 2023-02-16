e0fd0a7325516166e66f68bb52afb287b3814297
Laldingliana Tlau Vantawl <pateatlau@gmail.com>
2023-02-16 13:16:28 +0530

    Semantic UI Added

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1 className="title">Hello React with TypeScript</h1>
+      <button className="ui button">Click Me</button>
     </div>
   );
 }
