cefe786a26b6026570050cd794ac61f8a239bb0d
Laldingliana Tlau Vantawl <pateatlau@gmail.com>
2023-02-16 13:45:34 +0530

    Highcharts added

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,13 @@ import { AgGridReact } from 'ag-grid-react';
 import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-alpine.css';
 
+import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
+
 import './App.scss';
 
 function App() {
+  // Ag-grid section starts
   const [rowData] = useState([
     {make: "Toyota", model: "Celica", price: 35000},
     {make: "Ford", model: "Mondeo", price: 32000},
@@ -18,18 +22,44 @@ function App() {
     { field: 'model' },
     { field: 'price' }
   ]);
+  // Ag-grid section ends
+
+  // Highcharts section starts
+  const options = {
+    chart: {
+      type: 'spline'
+    },
+    title: {
+      text: 'My Highchart'
+    },
+    series: [
+      {
+        data: [1, 2, 1, 4, 3, 6]
+      }
+    ]
+  };
+  // Highcharts section ends
 
   return (
     <div className="App">
       <h1 className="title">Hello React with TypeScript</h1>
       <button className="ui button">Click Me</button>
-      <div className="ag-theme-alpine" style={{height: 400, width: 600}}>
+
+      {/* Ag-grid section starts */}
+      <div className="ag-theme-alpine" style={{height: 400, width: 605}}>
         <AgGridReact
           rowData={rowData}
           columnDefs={columnDefs}>
         </AgGridReact>
        </div>
-    </div>
+      {/* Ag-grid section ends */}
+
+      {/* Highcharts section starts */}
+      <div>
+        <HighchartsReact highcharts={Highcharts} options={options} />
+      </div>
+      {/* Highcharts section ends */}
+      </div>
   );
 }
 
