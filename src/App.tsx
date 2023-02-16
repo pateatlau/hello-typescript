787256bff401b72e5a32cfb9111b34864e609c15
Laldingliana Tlau Vantawl <pateatlau@gmail.com>
2023-02-16 13:27:52 +0530

    Ag-grid added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-alpine.css';
+
 import './App.scss';
 
 function App() {
+  const [rowData] = useState([
+    {make: "Toyota", model: "Celica", price: 35000},
+    {make: "Ford", model: "Mondeo", price: 32000},
+    {make: "Porsche", model: "Boxster", price: 72000}
+  ]);
+
+  const [columnDefs] = useState([
+    { field: 'make' },
+    { field: 'model' },
+    { field: 'price' }
+  ]);
+
   return (
     <div className="App">
       <h1 className="title">Hello React with TypeScript</h1>
       <button className="ui button">Click Me</button>
+      <div className="ag-theme-alpine" style={{height: 400, width: 600}}>
+        <AgGridReact
+          rowData={rowData}
+          columnDefs={columnDefs}>
+        </AgGridReact>
+       </div>
     </div>
   );
 }
